6b9294e7c7cabe01ae771c3f63579788884ccfd8
plaralei <princesslaralei@gmail.com>
2025-05-02 01:45:50 +0800

    UPDATED Model Package - InvestmentAccount

--- src/Group2BankSystem/model/InvestmentAccount.java
+++ src/Group2BankSystem/model/InvestmentAccount.java
@@ -2,38 +2,44 @@ package Group2BankSystem.model;
 
 import Group2BankSystem.exceptions.*;
 
-public class InvestmentAccount extends BankAccount {
+public abstract class InvestmentAccount extends BankAccount {
     private double interestRate;
     private double totalInterestEarned;
 
     public InvestmentAccount(String accountNumber, String accountHolderName, double initialDeposit)
             throws InvalidAmountException {
         super(accountNumber, accountHolderName, initialDeposit);
-        this.accountType = "Investment Account";
         this.interestRate = 0.05;
-        this.totalInterestEarned = 0;
+        this.minimumBalance = 500;
+        this.accountType = "Investment Account";
     }
-
-    public double getInterestRate() { return interestRate; }
-    public void setInterestRate(double rate) {
-        this.interestRate = rate;
-        updateLastModifiedDate();
-    }
-
-    public double getTotalInterestEarned() { return totalInterestEarned; }
 
     public void applyMonthlyInterest() throws AccountClosedException {
         if (!isActive) throw new AccountClosedException(accountNumber);
-
-        double interest = balance * (interestRate / 12);
+        double interest = balance * interestRate;
         balance += interest;
         totalInterestEarned += interest;
         updateLastModifiedDate();
         TransactionManager.addTransaction(accountNumber, "INTEREST", interest, "Monthly interest");
     }
 
-    public double computeMonthlyInterest() throws AccountClosedException {
-        if (!isActive) throw new AccountClosedException(accountNumber);
-        return balance * (interestRate / 12);
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public double getTotalInterestEarned() {
+        return totalInterestEarned;
+    }
+
+    @Override
+    public void setActive(boolean active) {
+        this.isActive = active;
+    }
+
+    @Override
+    protected void validateSufficientFunds(double amount) throws InsufficientFundsException {
+        if (balance - amount < minimumBalance) {
+            throw new InsufficientFundsException(balance - minimumBalance, amount);
+        }
     }
 }
